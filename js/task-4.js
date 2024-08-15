fa7f7864bca9e001d0f356f9d52541928ac3d655
Igor Tymchuk <167434833+Igor-Tymchuk@users.noreply.github.com>
2024-08-15 10:15:36 +0300

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const userForm = document.querySelector("form.login-form");
+userForm.addEventListener("submit", eventForm);
 
+function eventForm(event) {
+    event.preventDefault();
+    const form = event.target;
+    const userEmail = (form.elements.email.value).trim();
+    const keyEmail = form.elements.email.name;
+    const userPwd = (form.elements.password.value).trim();
+    const keyPwd = form.elements.password.value;
+    if(!userEmail || !userPwd) alert('All form fields must be filled in');
+    const result = {
+        keyEmail: userEmail,
+        keyPwd: userPwd,
+    }
+    console.log("result:", result)
+    form.reset();
+    return result;
+}
